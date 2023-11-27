827e89f50f389f319ba95817fe9e4beed7706f69
ruangkosong33 <ruangkosong33@ruangkosong33@gmail.com>
2023-11-27 17:03:49 +0800

    15

--- resources/views/layouts/admin/b-sidebar.blade.php
+++ resources/views/layouts/admin/b-sidebar.blade.php
@@ -7,22 +7,26 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{asset('bk/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+              @php
+                  $splitName = explode(' ',Auth::user()->username);
+              @endphp
+            <img src="https://ui-avatars.com/api/?name={{$splitName[0]}}&background=random" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+
+            <a href="javascript:;" class="d-block">{{Auth::user()->username}}</a>
+          </div>
         </div>
-        <div class="info">
-          <a href="#" class="d-block">{{auth()->user()->name}}</a>
-        </div>
-      </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          
+
           @if(Auth::user()->role == 'admin')
           <li class="nav-item">
             <a href="{{route('home')}}" class="nav-link {{request()->is('home*') ? 'active' : ''}}">
